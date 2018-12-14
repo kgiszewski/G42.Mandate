8b4104c326adcef291083a4601efca54b3237bcb
kgiszewski <kevin@giszewski.com>
2018-12-14 09:20:43 -0500

    Init

---
+++ src/Mandate/Mandate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace G42.Mandate
+{
+    public static class Mandate
+    {
+        public static void That<TException>(bool condition, string message) where TException : Exception
+        {
+            if (!condition)
+            {
+                var exceptionInstance = (TException)Activator.CreateInstance(typeof(TException), message);
+
+                throw exceptionInstance;
+            }
+        }
+    }
+}
